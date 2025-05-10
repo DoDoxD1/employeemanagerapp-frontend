3c13988cf0afec8cb3a53a1524b6d23173c1c49a
= <=>
2025-05-10 19:41:09 +0530

    feat: update .gitignore to include environment files and build artifacts
    fix: add file replacements for development environment in angular.json
    
    feat: implement employee listing in app.component.html
    
    feat: enhance AppComponent to fetch employees from EmployeeService
    
    feat: create EmployeeService for API interactions
    
    feat: define Employee interface for type safety
    
    feat: set up environment configuration for development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
